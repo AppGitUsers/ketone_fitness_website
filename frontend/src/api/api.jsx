53f159bbddd1427fee86f5763ba7eeb920067d67
AppGitUsers <appuser@sssedu.in>
2026-06-18 00:34:57 -0400

    server push

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = import.meta.env.VITE_API_BASE;
 
 export const getEnquiries = async () => {
   return await axios.get(`${API_URL}/enquiries/`);
